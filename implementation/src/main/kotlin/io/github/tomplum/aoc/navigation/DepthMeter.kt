90ba9d2f41bc481802e68dd84a85a326b21ff527
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-02 08:46:38 +0000

    Day 2 | Moved Directive Parsing -> Domain Companion Constructor

--- implementation/src/main/kotlin/io/github/tomplum/aoc/navigation/DepthMeter.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/navigation/DepthMeter.kt
@@ -2,19 +2,9 @@ package io.github.tomplum.aoc.navigation
 
 import io.github.tomplum.aoc.navigation.strategy.NavigationStrategy
 
-
 class DepthMeter(private val course: List<String>) {
     fun calculateCourseDestination(strategy: NavigationStrategy): PositionReport {
-        val directives = course.map { entry ->
-            val values = entry.split(" ")
-            when(values[0]) {
-                "forward" ->  Directive(Direction.FORWARD, values[1].toInt())
-                "down" ->  Directive(Direction.DOWN, values[1].toInt())
-                "up" ->  Directive(Direction.UP, values[1].toInt())
-                else -> throw IllegalArgumentException("Invalid Direction [$${values[0]}]")
-            }
-        }
-
+        val directives = course.map { entry -> Directive.fromString(entry) }
         return strategy.calculatePositionReport(directives)
     }
 }
--- implementation/src/main/kotlin/io/github/tomplum/aoc/navigation/Directive.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/navigation/Directive.kt
@@ -1,4 +1,18 @@
 package io.github.tomplum.aoc.navigation
 
-data class Directive(val direction: Direction, val distance: Int)
+data class Directive(val direction: Direction, val distance: Int) {
+    companion object {
+        fun fromString(value: String): Directive {
+            val values = value.split(" ")
+            val direction = values[0]
+            val distance = values[1].toInt()
+            return when(direction) {
+                "forward" ->  Directive(Direction.FORWARD, distance)
+                "down" ->  Directive(Direction.DOWN, distance)
+                "up" ->  Directive(Direction.UP, distance)
+                else -> throw IllegalArgumentException("Invalid Direction [$$direction]")
+            }
+        }
+    }
+}
 
