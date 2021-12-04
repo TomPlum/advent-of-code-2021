5eea8d22cdac5fb157a114e888de1920920a81c0
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-04 08:13:47 +0000

    Day 4 | Added Part 1 Solution

--- solutions/src/main/kotlin/io/github/tomplum/aoc/RunSolutions.kt
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/RunSolutions.kt
@@ -3,8 +3,9 @@ package io.github.tomplum.aoc
 import io.github.tomplum.aoc.solutions.Day1
 import io.github.tomplum.aoc.solutions.Day2
 import io.github.tomplum.aoc.solutions.Day3
+import io.github.tomplum.aoc.solutions.Day4
 import io.github.tomplum.libs.solutions.SolutionRunner
 
 fun main() {
-    SolutionRunner.run(Day1(), Day2(), Day3())
+    SolutionRunner.run(Day1(), Day2(), Day3(), Day4())
 }
---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day4.kt
@@ -0,0 +1,14 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.bingo.Bingo
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day4 : Solution<Int, Int> {
+    override fun part1(): Int {
+        val input = InputReader.read<String>(Day(4)).value
+        val bingo = Bingo(input)
+        return bingo.play()
+    }
+}
