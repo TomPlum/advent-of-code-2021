caae6b8a3448cbd9620ea9c6881361204ee1f85c
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-06 08:54:26 +0000

    Day 6 | Minor Improvements & Formatting

--- implementation/src/main/kotlin/io/github/tomplum/aoc/fish/LanternFishSimulator.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/fish/LanternFishSimulator.kt
@@ -3,16 +3,22 @@ package io.github.tomplum.aoc.fish
 class LanternFishSimulator(private val data: String) {
     fun simulate(days: Int): Long {
         val fish = data.trim().split(",")
-            .map { value -> value.toInt() }
             .groupBy { age -> age }
-            .map { (age, quantity) -> age to quantity.size.toLong() }
+            .map { (age, quantity) -> age.toInt() to quantity.size.toLong() }
             .toMap().toMutableMap()
 
         repeat(days) {
-            val newAges = fish.map { (age, quantity) -> (if (age == 0) 6 else age - 1) to quantity }
+            val newAges = fish.map { (age, quantity) ->
+                (if (age == 0) 6 else age - 1) to quantity
+            }
+
             val newFish = (8 to (fish[0] ?: 0))
+
             fish.clear()
-            (newAges + newFish).forEach { (age, quantity) -> fish[age] = (fish[age] ?: 0) + quantity}
+
+            (newAges + newFish).forEach { (age, quantity) ->
+                fish[age] = (fish[age] ?: 0) + quantity
+            }
         }
 
         return fish.values.sum()
