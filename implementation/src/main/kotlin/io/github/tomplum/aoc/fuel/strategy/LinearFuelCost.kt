323fd102f2ceb75918b726530217f32995614e40
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-07 08:37:33 +0000

    Day 7 | Refactored Linear Fuel Cost Strategy

--- implementation/src/main/kotlin/io/github/tomplum/aoc/fuel/strategy/LinearFuelCost.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/fuel/strategy/LinearFuelCost.kt
@@ -1,21 +1,9 @@
 package io.github.tomplum.aoc.fuel.strategy
 
-import io.github.tomplum.libs.logging.AdventLogger
 import kotlin.math.abs
 
 class LinearFuelCost : FuelCostStrategy {
-    override fun calculateCheapestHorizontalAlignment(positions: List<Int>): Int {
-        var cheapest = Integer.MAX_VALUE
-        positions.forEach { target ->
-            val cost = positions.fold(0) { total, pos ->
-                val cost = abs(target - pos)
-                AdventLogger.debug("Move from $target to $pos: $cost fuel")
-                total + cost
-            }
-            if (cost < cheapest) {
-                cheapest = cost
-            }
-        }
-        return cheapest
-    }
+    override fun calculateCheapestHorizontalAlignment(positions: List<Int>) = positions.minOfOrNull { target ->
+        positions.fold(0) { total, pos -> total + abs(target - pos) }
+    } ?: 0
 }
