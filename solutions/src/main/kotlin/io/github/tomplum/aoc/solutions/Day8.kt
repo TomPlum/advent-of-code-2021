d085d94324efa9037a83fde36f5eb425f752bb1d
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-08 10:16:11 +0000

    Day 8 | Reduced Solution Redundancy

--- solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day8.kt
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day8.kt
@@ -6,15 +6,14 @@ import io.github.tomplum.libs.input.InputReader
 import io.github.tomplum.libs.solutions.Solution
 
 class Day8 : Solution<Int, Int> {
+    private val input = InputReader.read<String>(Day(8)).value
+    private val analyser = DisplayAnalyser(input)
+
     override fun part1(): Int {
-        val input = InputReader.read<String>(Day(8)).value
-        val analyser = DisplayAnalyser(input)
         return analyser.countUniqueSegmentsInstances()
     }
 
     override fun part2(): Int {
-        val input = InputReader.read<String>(Day(8)).value
-        val analyser = DisplayAnalyser(input)
         return analyser.getOutputValueSum()
     }
 }
