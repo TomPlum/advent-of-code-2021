ccfff997aaa29a5cc859ca0fa9e9414dfd48965c
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-09 07:46:34 +0000

    Day 9 | Added Part 1 Solution

---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day9.kt
@@ -0,0 +1,14 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.map.SafetyModule
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day9 : Solution<Int, Int> {
+    override fun part1(): Int {
+        val input = InputReader.read<String>(Day(9)).value
+        val safetyModule = SafetyModule(input)
+        return safetyModule.calculateRiskLevel()
+    }
+}
