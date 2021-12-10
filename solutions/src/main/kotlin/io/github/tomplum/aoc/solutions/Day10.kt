f640d69d69a67f50aea66834fbea64b85154427f
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-10 07:41:00 +0000

    Day 10 | Added Part 1 Solution

---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day10.kt
@@ -0,0 +1,14 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.navigation.subsystem.NavigationSubsystem
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day10 : Solution<Int, Int> {
+    override fun part1(): Int {
+        val input = InputReader.read<String>(Day(10)).value
+        val subsystem = NavigationSubsystem(input)
+        return subsystem.getTotalSyntaxErrorScore()
+    }
+}
