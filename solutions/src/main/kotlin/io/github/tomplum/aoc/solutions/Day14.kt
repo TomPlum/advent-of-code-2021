3b0f0895f5765b045deb254c90bcbe671fa64f06
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-14 21:43:39 +0000

    Day 14 | Added Part 2 Solution

--- solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day14.kt
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day14.kt
@@ -5,10 +5,16 @@ import io.github.tomplum.libs.input.Day
 import io.github.tomplum.libs.input.InputReader
 import io.github.tomplum.libs.solutions.Solution
 
-class Day14 : Solution<Int, Int> {
-    override fun part1(): Int {
+class Day14 : Solution<Long, Long> {
+    override fun part1(): Long {
         val input = InputReader.read<String>(Day(14)).value
         val polymeriser = Polymeriser(input)
-        return polymeriser.doThing(10) //2604 too high
+        return polymeriser.doThing(10)
+    }
+
+    override fun part2(): Long {
+        val input = InputReader.read<String>(Day(14)).value
+        val polymeriser = Polymeriser(input)
+        return polymeriser.doThing(40)
     }
 }
--- solutions/src/test/kotlin/io/github/tomplum/aoc/solutions/Day14Test.kt
+++ solutions/src/test/kotlin/io/github/tomplum/aoc/solutions/Day14Test.kt
@@ -11,4 +11,9 @@ class Day14Test {
     fun partOne() {
         assertThat(solution.part1()).isEqualTo(2602)
     }
+
+    @Test
+    fun partTwo() {
+        assertThat(solution.part2()).isEqualTo(2942885922173)
+    }
 }
