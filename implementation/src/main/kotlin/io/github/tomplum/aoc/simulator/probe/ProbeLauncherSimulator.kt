b4582e9ecfc3ddd0899f455904204b180a3b5cc5
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-17 10:19:35 +0000

    Day 17 | Reduced Implementation Redundancy & Cleaned Test Suite

--- implementation/src/main/kotlin/io/github/tomplum/aoc/simulator/probe/ProbeLauncherSimulator.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/simulator/probe/ProbeLauncherSimulator.kt
@@ -2,30 +2,29 @@ package io.github.tomplum.aoc.simulator.probe
 
 import io.github.tomplum.libs.math.point.Point2D
 
-class ProbeLauncherSimulator(private val target: String) {
+class ProbeLauncherSimulator(target: String) {
 
     private val xMin: Int
     private val xMax: Int
     private val yMin: Int
     private val yMax: Int
 
+    private val yOrdinates = mutableSetOf<Int>()
+    private val validStartingVelocity = mutableSetOf<Point2D>()
+
     init {
         val coords = target.removePrefix("target area: ").split(", ")
 
-        val x = coords[0].removePrefix("x=").split("..")
-        xMin = x[0].toInt()
-        xMax = x[1].toInt()
+        val xArea = coords[0].removePrefix("x=").split("..")
+        xMin = xArea[0].toInt()
+        xMax = xArea[1].toInt()
 
-        val y = coords[1].removePrefix("y=").split("..")
-        yMin = y[0].toInt()
-        yMax = y[1].toInt()
-    }
+        val yArea = coords[1].removePrefix("y=").split("..")
+        yMin = yArea[0].toInt()
+        yMax = yArea[1].toInt()
 
-    fun calculateMaximumVerticalHeight(): Int {
-        val yOrdinates = mutableSetOf<Int>()
-
-        for (xStartVelocity in 1 until 100) {
-            for (yStartVelocity in 1 until 100) {
+        for (xStartVelocity in 1 until 1000) {
+            for (yStartVelocity in -100 until 1000) {
 
                 val yTrajectory = mutableSetOf<Int>()
                 var xVelocity = xStartVelocity
@@ -44,38 +43,17 @@ class ProbeLauncherSimulator(private val target: String) {
 
                 if (isWithinTargetArea(x, y)) {
                     yOrdinates.addAll(yTrajectory)
-                }
-            }
-        }
-
-        return yOrdinates.maxOrNull() ?: 0
-    }
-
-    fun calculateTotalDistinctInitialVelocityValues(): Set<Point2D> {
-        val validStartingVelocity = mutableSetOf<Point2D>()
-
-        for (xStartVelocity in -50 until 1000) {
-            for (yStartVelocity in -100 until 1000) {
-
-                var xVelocity = xStartVelocity
-                var yVelocity = yStartVelocity
-
-                var x = 0
-                var y = 0
-
-                while (!isWithinTargetArea(x, y) && !hasMissedTargetArea(x, y)) {
-                    x += xVelocity
-                    y += yVelocity
-                    xVelocity = if (xVelocity > 0) xVelocity - 1 else if (xVelocity < 0) xVelocity + 1 else 0
-                    yVelocity -= 1
-                }
-
-                if (isWithinTargetArea(x, y)) {
                     validStartingVelocity.add(Point2D(xStartVelocity, yStartVelocity))
                 }
             }
         }
-        // 232 too low
+    }
+
+    fun calculateMaximumVerticalHeight(): Int {
+        return yOrdinates.maxOrNull() ?: 0
+    }
+
+    fun calculateTotalDistinctInitialVelocityValues(): Set<Point2D> {
         return validStartingVelocity
     }
 
--- implementation/src/test/kotlin/io/github/tomplum/aoc/simulator/probe/ProbeLauncherSimulatorTest.kt
+++ implementation/src/test/kotlin/io/github/tomplum/aoc/simulator/probe/ProbeLauncherSimulatorTest.kt
@@ -3,7 +3,6 @@ package io.github.tomplum.aoc.simulator.probe
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import io.github.tomplum.aoc.input.TestInputReader
-import io.github.tomplum.libs.math.point.Point2D
 import org.junit.jupiter.api.Test
 
 class ProbeLauncherSimulatorTest {
@@ -16,14 +15,8 @@ class ProbeLauncherSimulatorTest {
 
     @Test
     fun exampleTwo() {
-        val ordinates = TestInputReader.read<String>("/day17/test.txt").value.flatMap { line ->
-            line.split("@").map { pair -> pair.split(",").let { values -> Point2D(values[0].toInt(), values[1].toInt()) } }
-        }
         val input = TestInputReader.read<String>("/day17/example.txt").asSingleString()
         val simulator = ProbeLauncherSimulator(input)
-        val validPoints = simulator.calculateTotalDistinctInitialVelocityValues()
-        val diff = ordinates.minus(validPoints)
-        val s = ""
-        assertThat(validPoints.size).isEqualTo(112)
+        assertThat(simulator.calculateTotalDistinctInitialVelocityValues().size).isEqualTo(112)
     }
 }
