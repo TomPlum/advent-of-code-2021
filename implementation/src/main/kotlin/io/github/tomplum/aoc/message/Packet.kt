9e6f5bf7f987bbc8698ce378556277f86ae69319
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-18 21:33:22 +0000

    Day 16 | Refactored Packet -> Proper Tree Structure

--- implementation/src/main/kotlin/io/github/tomplum/aoc/message/Packet.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/message/Packet.kt
@@ -11,53 +11,55 @@ data class Packet(val data: String) {
     val type = if (Integer.parseInt(data.substring(3..5), 2) == 4) LITERAL else OPERATOR
     val lengthType = LengthType.fromInteger(data[6].toString().toInt())
     var literalValue: Int? = null
+    val children = mutableListOf<Packet>()
 
-    fun getSubPackets(): List<Packet> = when(type) {
-        LITERAL -> {
-            /*var padded = data.substring(6)
-            while (padded.length % 4 != 0) {
-                padded += "0"
-            }*/
-            val chunks = data.substring(6).chunked(5) //.filter { chunk -> !chunk.all { char -> char == '0' } }
-            val valueChunks = chunks.takeWhile { chunk -> chunk.first() == '1' }.toMutableList()
-            if (valueChunks.isEmpty()) {
-                valueChunks.add(chunks.first())
-            } else {
-                valueChunks.add(chunks[valueChunks.size])
-            }
-            val digits = valueChunks.joinToString("") { chunk -> chunk.substring(1) }
-            //literalValue = Integer.parseInt(digits, 2)
+/*    fun calculateValue() = when(lengthType) {
+        SUM -> {
 
-            val potentialPacketData = data.substring(7 + valueChunks.joinToString("").length - 1)
-            if (!potentialPacketData.all { char -> char == '0' }) {
-                listOf(this) + Packet(potentialPacketData).getSubPackets()
-            } else {
-                listOf(this)
-            }
         }
-        OPERATOR -> {
-            when(lengthType) {
-                SUM -> {
-                    val subPacketLength = Integer.parseInt(data.substring(7..21), 2)
-                    val subPacketData = data.substring(22, 22 + subPacketLength)
-                    val otherPacketData = data.substring(22 + subPacketLength).dropLastWhile { it == '0' }
-                    if (otherPacketData.isNotBlank()) {
-                        Packet(subPacketData).getSubPackets() + Packet(otherPacketData).getSubPackets() + this
-                    } else {
-                        Packet(subPacketData).getSubPackets() + this
+    }*/
+
+    init {
+        when(type) {
+            LITERAL -> {
+                val chunks = data.substring(6).chunked(5)
+                val valueChunks = chunks.takeWhile { chunk -> chunk.first() == '1' }.toMutableList()
+                if (valueChunks.isEmpty()) {
+                    valueChunks.add(chunks.first())
+                } else {
+                    valueChunks.add(chunks[valueChunks.size])
+                }
+                val digits = valueChunks.joinToString("") { chunk -> chunk.substring(1) }
+                //literalValue = Integer.parseInt(digits, 2)
+
+                val potentialPacketData = data.substring(7 + valueChunks.joinToString("").length - 1)
+                if (!potentialPacketData.all { char -> char == '0' }) {
+                    children.add(Packet(potentialPacketData))
+                }
+            }
+            OPERATOR -> {
+                when(lengthType) {
+                    SUM -> {
+                        val subPacketLength = Integer.parseInt(data.substring(7..21), 2)
+                        val subPacketData = data.substring(22, 22 + subPacketLength)
+                        val otherPacketData = data.substring(22 + subPacketLength).dropLastWhile { it == '0' }
+                        if (otherPacketData.isNotBlank()) {
+                            children.addAll(listOf(Packet(subPacketData), Packet(otherPacketData)))
+                        } else {
+                            children.add(Packet(subPacketData))
+                        }
+                    }
+                    PRODUCT -> {
+                        val subPacketData = data.substring(18).dropLastWhile { value -> value == '0' }
+                        children.add(Packet(subPacketData))
                     }
                 }
-                PRODUCT -> {
-                    val subPacketQuantity = Integer.parseInt(data.substring(8..17), 2)
-                    val subPacketData = data.substring(18).dropLastWhile { value -> value == '0' }
-                   /* subPacketData.dropLastWhile { value -> value == '0' }.chunked(subPacketData.length / subPacketQuantity).flatMap { binary ->
-                        Packet(binary).getSubPackets() + this
-                    }*/
-                    Packet(subPacketData).getSubPackets() + this
-                }
-                else -> emptyList()
             }
         }
+    }
+
+    fun getSubPackets(): List<Packet> {
+        return children + children.flatMap { packet -> packet.getSubPackets() }
     }
 
     override fun equals(other: Any?): Boolean {
--- implementation/src/main/kotlin/io/github/tomplum/aoc/message/PacketDecoder.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/message/PacketDecoder.kt
@@ -24,8 +24,8 @@ class PacketDecoder(private val transmission: String) {
     fun calculatePacketVersionNumberSum(): Int {
         val binary = transmission.map { char -> binaryMapping[char] }.joinToString("")
         val outermostPacket = Packet(binary)
-        val packets = outermostPacket.getSubPackets()
-        return packets.distinct().sumOf { packet -> packet.version }
+        val packets = outermostPacket.getSubPackets() + outermostPacket
+        return packets.sumOf { packet -> packet.version }
     }
 
     fun evaluateExpressionValue(): Int {
