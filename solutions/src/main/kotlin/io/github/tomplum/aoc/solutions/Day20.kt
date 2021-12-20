d3408b13f4b62c81379ffd1b37b80dd578834b61
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-20 20:59:43 +0000

    Day 20 | Reduced Solution Redundancy

--- solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day20.kt
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day20.kt
@@ -6,15 +6,14 @@ import io.github.tomplum.libs.input.InputReader
 import io.github.tomplum.libs.solutions.Solution
 
 class Day20 : Solution<Int, Int> {
+    private val input = InputReader.read<String>(Day(20)).value
+    private val scanner = Scanner(input)
+
     override fun part1(): Int {
-        val input = InputReader.read<String>(Day(20)).value
-        val scanner = Scanner(input)
         return scanner.enhanceImage(2)
     }
 
     override fun part2(): Int {
-        val input = InputReader.read<String>(Day(20)).value
-        val scanner = Scanner(input)
         return scanner.enhanceImage(50)
     }
 }
