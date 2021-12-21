ac499404d7993dc1b7a1f76d7b8257218d839619
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-21 17:21:51 +0000

    Day 21 | Added Solution Part 1

---
+++ solutions/src/main/kotlin/io/github/tomplum/aoc/solutions/Day21.kt
@@ -0,0 +1,14 @@
+package io.github.tomplum.aoc.solutions
+
+import io.github.tomplum.aoc.game.DiracDice
+import io.github.tomplum.libs.input.Day
+import io.github.tomplum.libs.input.InputReader
+import io.github.tomplum.libs.solutions.Solution
+
+class Day21 : Solution<Int, Int> {
+    override fun part1(): Int {
+        val input = InputReader.read<String>(Day(21)).value
+        val game = DiracDice(input)
+        return game.play()
+    }
+}
