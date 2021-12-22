b3936c5324f6dcc0461050c1ad72e911b37490aa
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-12-22 17:10:35 +0000

    Day 21 | Extracted Quantum Die Rolling Redundancy

--- implementation/src/main/kotlin/io/github/tomplum/aoc/game/DiracDice.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/game/DiracDice.kt
@@ -27,28 +27,9 @@ class DiracDice(data: List<String>) {
         return die.rolls * minOf(playerOne.score, playerTwo.score)
     }
 
-    fun playWithQuantumDice(): Long {
-        var playerOneWins = 0L
-        var playerTwoWins = 0L
-
-        playerOneWins += roll(playerOneStart, playerTwoStart, 0, 0, 9, true, 1)
-        playerOneWins += roll(playerOneStart, playerTwoStart, 0, 0, 8, true, 1) * 3
-        playerOneWins += roll(playerOneStart, playerTwoStart, 0, 0, 7, true, 1) * 6
-        playerOneWins += roll(playerOneStart, playerTwoStart, 0, 0, 6, true, 1) * 7
-        playerOneWins += roll(playerOneStart, playerTwoStart, 0, 0, 5, true, 1) * 6
-        playerOneWins += roll(playerOneStart, playerTwoStart, 0, 0, 4, true, 1) * 3
-        playerOneWins += roll(playerOneStart, playerTwoStart, 0, 0, 3, true, 1)
-
-        playerTwoWins += roll(playerOneStart, playerTwoStart, 0, 0, 9, true, 2)
-        playerTwoWins += roll(playerOneStart, playerTwoStart, 0, 0, 8, true, 2) * 3
-        playerTwoWins += roll(playerOneStart, playerTwoStart, 0, 0, 7, true, 2) * 6
-        playerTwoWins += roll(playerOneStart, playerTwoStart, 0, 0, 6, true, 2) * 7
-        playerTwoWins += roll(playerOneStart, playerTwoStart, 0, 0, 5, true, 2) * 6
-        playerTwoWins += roll(playerOneStart, playerTwoStart, 0, 0, 4, true, 2) * 3
-        playerTwoWins += roll(playerOneStart, playerTwoStart, 0, 0, 3, true, 2)
-
-        return maxOf(playerOneWins, playerTwoWins)
-    }
+    fun playWithQuantumDice(): Long = quantumDie.forEachRollPossibility { value ->
+        roll(playerOneStart, playerTwoStart, 0, 0, value, true)
+    }.let { (playerOneWins, playerTwoWins) -> maxOf(playerOneWins, playerTwoWins) }
 
     fun playWithQuantumDice2(): Long {
         var playerOneWins = 0L
@@ -107,13 +88,13 @@ class DiracDice(data: List<String>) {
         return wins
     }
 
-    fun roll(p1: Int, p2: Int, p1Score: Int, p2Score: Int, roll: Int, p1Turn: Boolean, player: Int): Long {
+    fun roll(p1: Int, p2: Int, p1Score: Int, p2Score: Int, roll: Int, p1Turn: Boolean): Pair<Long, Long> {
         var playerOnePos = p1
         var playerTwoPos = p2
         var playerOneScore = p1Score
         var playerTwoScore = p2Score
 
-        var wins = 0L
+        var wins = Pair(0L, 0L)
 
         if (p1Turn) {
             playerOnePos += roll
@@ -123,18 +104,15 @@ class DiracDice(data: List<String>) {
             playerTwoScore += (playerTwoPos - 1) % 10 + 1
         }
 
-
         if (playerOneScore >= 21 || playerTwoScore >= 21) {
-            return if (player == 1) {
-                if (playerOneScore >= 21) 1 else 0
-            } else {
-                if (playerTwoScore >= 21) 1 else 0
-            }
+            return if (playerOneScore >= 21) Pair(1, 0) else Pair(0, 1)
         } else {
-            wins += quantumDie.forEachRollPossibility { value ->
-                roll(playerOnePos, playerTwoPos, playerOneScore, playerTwoScore, value, !p1Turn, player)
+            val otherWins = quantumDie.forEachRollPossibility { value ->
+                roll(playerOnePos, playerTwoPos, playerOneScore, playerTwoScore, value, !p1Turn)
             }
+            wins = Pair(wins.first + otherWins.first, wins.second + otherWins.second)
         }
+
         return wins
     }
 }
--- implementation/src/main/kotlin/io/github/tomplum/aoc/game/QuantumDie.kt
+++ implementation/src/main/kotlin/io/github/tomplum/aoc/game/QuantumDie.kt
@@ -1,15 +1,30 @@
 package io.github.tomplum.aoc.game
 
 class QuantumDie {
-    fun forEachRollPossibility(function: (value: Int) -> Long): Long {
-        var total = 0L
-        total += function(9)
-        total += function(8) * 3
-        total += function(7) * 6
-        total += function(6) * 7
-        total += function(5) * 6
-        total += function(4) * 3
-        total += function(3)
+    fun forEachRollPossibility(function: (value: Int) -> Pair<Long, Long>): Pair<Long, Long> {
+        var total = Pair(0L, 0L)
+
+        val nine = function(9)
+        total = Pair(total.first + nine.first, total.second + nine.second)
+
+        val eight = function(8)
+        total = Pair(total.first + eight.first * 3, total.second + eight.second * 3)
+
+        val seven = function(7)
+        total = Pair(total.first + seven.first * 6, total.second + seven.second * 6)
+
+        val six = function(6)
+        total = Pair(total.first + six.first * 7, total.second + six.second * 7)
+
+        val five = function(5)
+        total = Pair(total.first + five.first * 6, total.second + five.second * 6)
+
+        val four = function(4)
+        total = Pair(total.first + four.first * 3, total.second + four.second * 3)
+
+        val three = function(3)
+        total = Pair(total.first + three.first, total.second + three.second)
+
         return total
     }
 }
